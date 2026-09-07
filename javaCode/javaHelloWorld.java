48e75e93f9e3e67a5f7a84de69ac29120fc283a3
bnnwat42 <157958622+bnnwat42@users.noreplay.github.com>
2026-09-07 00:20:02 -0600

    You can now add your name as an argument

--- javaCode/javaHelloWorld.java
+++ javaCode/javaHelloWorld.java
@@ -1,7 +1,11 @@
 public class javaHelloWorld {
     public static void main(String[] args){
+        var name = "[NAME]";
+        if (args.length != 0) {
+            name = args[0];
+        }
         for(int i=0; i<10; i++){
-            System.out.println(i + " time saying this.");
+            System.out.println(i + " time saying \"Hi " + name + "!\".");
         }
     }
 }
